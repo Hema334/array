139f8514567f090d6cb2c8a33cb0b5ba872297f8
Hema334 <112267828+Hema334@users.noreply.github.com>
2024-09-08 20:52:48 +0530

    Add files via upload

---
+++ array.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <stdio.h>
+void main()
+{
+int height[50];
+height[2]=45;
+height[5]=88;
+height[7]=76;
+printf("%d",height[7]);
+printf("%d",height[5]);
+printf("%d",height[2]);
+}
